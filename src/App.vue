ef352de2057f4066a4d3bed11d86c935945fb786
kodeman2 <holuwakoladey@gmail.com>
2023-09-19 23:57:29 +0100

    vue ehealth

---
+++ src/App.vue
@@ -0,0 +1,241 @@
+
+ <template>
+  <div class="app">
+   <div class="header">
+      <h1>MEDICharts</h1>
+      <p>Charts of Blood Group and Age Range</p>
+   </div>
+   <div class="charts">
+
+    <highcharts :options="bloodGroupChartOptions"></highcharts>
+    <highcharts :options="ageRangeChartOptions"></highcharts>
+   </div>
+
+    <div class="footer">
+    <a href=" "><button>Repo</button></a> 
+     <p>@kodeman 2023</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { Chart } from "highcharts-vue";
+
+export default {
+  name: "ChartsComponent",
+  components: {
+    highcharts: Chart,
+  },
+
+  data() {
+    return {
+      bloodGroupChartOptions: {
+        chart: {
+          type: "bar",
+        },
+        title: {
+          text: "Blood Group",
+        },
+
+        subtitle: {
+          text: "Source: eHealth4EveryOne",
+        },
+        labels: {
+            rotation: 0,
+            style: {
+              fontSize: '13px',
+              fontFamily: 'Verdana, sans-serif'
+            }
+          },
+        xAxis: {
+          title: {
+            text: "Blood Group",
+          },
+         
+          categories: [],
+        },
+        yAxis: {
+          title: {
+            text: "Number of People",
+          },
+        },
+        series: [
+          {
+            name: "Number of People",
+            data: [],
+            color: '#FF0000',
+            allowPointSelect: true,
+            cursor: 'pointer',
+            
+            
+            
+          },
+        ],
+      },
+
+      ageRangeChartOptions: {
+        chart: {
+          type: "bar",
+        },
+        title: {
+          text: "Age Range",
+        },
+        subtitle: {
+          text: "Source: eHealth4EveryOne",
+        },
+        labels: {
+            rotation: 0,
+            style: {
+              fontSize: '13px',
+              fontFamily: 'Verdana, sans-serif'
+            }
+          },
+
+        
+        xAxis: {
+          title: {
+            text: "Age Range",
+           
+          },
+          categories: ["Ages 1-17", "Ages 18-29", "Ages 30+"],
+        },
+        yAxis: {
+          title: {
+            text: "Number of People",
+          },
+        },
+        series: [
+          {
+            name: "Number of People",
+            data: [],
+            color: '#008000',
+            allowPointSelect: true,
+            cursor: 'pointer',
+            
+          },
+        ],
+      },
+    };
+  },
+
+  mounted() {
+  this.fetchData();
+},
+methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get('https://ehealth4every1-1d367-default-rtdb.firebaseio.com/people.json');
+        const data = response.data;
+
+        if (typeof data === 'object') {
+         // Log the data
+         console.log('Data from API:', data);
+          // Convert the object into an array of objects
+          const dataArray = Object.values(data);
+          this.formatData(dataArray);
+          localStorage.setItem('Bloodgroup', JSON.stringify(dataArray));
+        } else {
+          console.error('Data is not an object:', data);
+        }
+      } catch (error) {
+        console.error(error);
+        const storedData = localStorage.getItem('Bloodgroup');
+        if (storedData) {
+          const parsedData = JSON.parse(storedData);
+          this.formatData(parsedData);
+        }
+      }
+    },
+
+    formatData(data) {
+      const bloodGroups = {};
+      const ageGroups = [0, 0, 0]; // [Children, Young Adults, Adults]
+
+      data.forEach(item => {
+        const bloodType = item.blood_group;
+        const age = item.age;
+
+        // Count blood groups
+        if (bloodGroups[bloodType]) {
+          bloodGroups[bloodType]++;
+        } else {
+          bloodGroups[bloodType] = 1;
+        }
+
+        // Categorize age groups
+        if (age >= 1 && age <= 17) {
+          ageGroups[0]++;
+        } else if (age >= 18 && age <= 29) {
+          ageGroups[1]++;
+        } else {
+          ageGroups[2]++;
+        }
+      });
+
+      // Prepare data for chart
+      const bloodGroupCategories = Object.keys(bloodGroups);
+      const bloodGroupCounts = Object.values(bloodGroups);
+
+      // Update chart data
+      this.bloodGroupChartOptions.xAxis.categories = bloodGroupCategories;
+      this.bloodGroupChartOptions.series[0].data = bloodGroupCounts;
+
+      // Update age range data
+      this.ageRangeChartOptions.series[0].data = ageGroups;
+    },
+  },
+
+
+};
+
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+.header {
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+  padding: 10px 16px;
+  background: #555;
+  color: #f1f1f1;
+  font-size: 20px;
+  font-weight: bold;
+  font-family: 'Courier New', Courier, monospace;
+  }
+  .charts{
+   display: flex;
+   flex-direction: column;
+   
+   padding: 10px 20px;
+   gap: 50px;
+  }
+  .footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 16px;
+    background: #dad6d6;
+    color: #020202;
+    font-size: 20px;
+    font-weight: bold;
+    font-family: 'Courier New', Courier, monospace;
+  }
+
+@media screen and (max-width: 600px) {
+ 
+  .header{
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+}
+
+ 
+
+
+</style>
